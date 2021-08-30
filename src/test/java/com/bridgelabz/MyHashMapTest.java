5e0ccce65164b2a9b82b9a3aaff55017cefbf9cc
Sunil Gollapalli <sunigollapalli19@gmail.com>
2021-08-30 22:52:38 +0530

    Added:Frequency of words in a Paragraph

--- src/test/java/com/bridgelabz/MyHashMapTest.java
+++ src/test/java/com/bridgelabz/MyHashMapTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 /**
  * purpose - to Implement the HashTable Method
+ *
  * @author Sunil
  * @version 16.0
  * @since 24/08/2021
@@ -27,5 +28,25 @@ public class MyHashMapTest {
         int frequency = myHashMap.get("to");
         System.out.println(myHashMap);
         Assertions.assertEquals(2, frequency);
+    }
+
+    @Test
+    public void givenASentence_whenWordsAreAddedToList_shouldReturnParanoidWordFrequency() {
+        String sentence = "Paranoids are not paranoid because they are paranoid but " +
+                "because they keep putting themselves deliberately " +
+                "into paranoid avoidable situations";
+        MyHashMapParanoid<String, Integer> myLinkedHashMap = new MyHashMapParanoid<>();
+        String[] words = sentence.toLowerCase().split(" ");
+        for (String word : words) {
+            Integer value = myLinkedHashMap.get(word);
+            if (value == null)
+                value = 1;
+            else
+                value += 1;
+            myLinkedHashMap.add(word, value);
+        }
+        System.out.println(myLinkedHashMap);
+        int frequency = myLinkedHashMap.get("paranoid");
+        Assertions.assertEquals(3, frequency);
     }
 }
